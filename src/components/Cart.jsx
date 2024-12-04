23ad474b726b1c05cc290b30b718231b7cc71a02
Akio Kane <aakocherga@mail.ru>
2024-12-05 00:38:02 +0700

    Added cart styles

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,11 +5,18 @@ import "../styles/Cart.css";
 
 function Cart() {
   const { setCartList, cartList } = useOutletContext();
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    const newTotal = cartList.reduce((acc, el) => acc + el.price, 0);
+    setTotal(newTotal);
+  }, [cartList])
 
   return (
     <>
       <div className="cart-content">
         <div className="content-cart">
+          <h1 className="head-cart">Cart</h1>
           {cartList ? cartList.map((_, index) => {
             return (
               <Card key={index}
@@ -20,6 +27,10 @@ function Cart() {
               />
             )
           }) : ""}
+        </div>
+        <div className="order-bar">
+          <h1>Total:</h1>
+          <h1>{`$ ${total.toLocaleString('en-US')}`}</h1>
         </div>
       </div>
     </>
